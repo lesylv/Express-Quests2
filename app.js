0947f908794a1e8a3f1bc9d9b156e4be2e990eb9
Tormo <sylvaain11@hotmail.fr>
2022-12-19 10:50:52 +0100

    Fin 02

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
@@ -16,11 +18,13 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovies);
 
 const usersHandlers = require("./usersHandlers");
 
 app.get("/api/users", usersHandlers.getUsers)
 app.get("/api/users/:id", usersHandlers.getUsersById);
+app.post("/api/users", usersHandlers.postUsers);
 
 app.listen(port, (err) => {
   if (err) {
